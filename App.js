6cd3c1d4a8c75cf3935b97424b6e1a3759df0e89
dipika24baranwal <58095307+dipika24baranwal@users.noreply.github.com>
2025-05-19 23:03:16 +0530

    Episode-02 changes

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent= React.createElement("div", {id:"parent"}, 
     [React.createElement("div", {id:"child"}, 
     React.createElement("h1", {id:"heading"}, "Hello World from React H1 tag!!!")
   ),
 React.createElement("div", {id:"child2"},
-    [React.createElement("h1", {id:"heading1"}, "Hello World from React H1 tag!!!"),
-        React.createElement("h2", {id:"heading2"}, "Hello World from React H2 tag!!!")
+    [React.createElement("h1", {}, "Hello World from React H1 tag!!!"),
+        React.createElement("h2", {}, "Hello World from React H2 tag!!!")
     ] 
     
   )]
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Namaste React
+
+# Parcel https://parceljs.org/
+    - Dev build
+    - Local server
+    - HMR: Hot Module replacement
+    - File watching algorithm - written in C++
+    - Caching: Faster Builds caching build in .parcel-cache folder
+    - Image Optimization
+    - Minification
+    - Bundling
+    - Compress
+    - Content Hashing:  
+    - Code splitting
+    - Differential Bundling: Support older browsers
+    - Diagnostic: give error properly
+    - Error Handling
+    - Host on https
+    - Tree shaking: remove unused code
+    - Different dev and prod bundles
+    
+
+
